6dc77fc46798ec2c5af32c5ba50420912fb3ea95
Ankit Yadav <48759185+ceoankityadav@users.noreply.github.com>
2021-12-17 21:22:58 +0530

    Time: 84 ms (27.62%), Space: 38.7 MB (78.23%) - LeetHub

---
+++ remove-element/remove-element.js
@@ -0,0 +1,14 @@
+/**
+ * @param {number[]} nums
+ * @param {number} val
+ * @return {number}
+ */
+var removeElement = function(nums, val) {
+    var i = 0;
+    while(i < nums.length){
+        if(nums[i] === val){
+            nums.splice(i,1);
+        }
+        else i++
+    }
+};
