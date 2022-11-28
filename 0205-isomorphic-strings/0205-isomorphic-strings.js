9553d85133c283ffbdfc1a4ef4d0aae3f307224f
Ankit Kumar <ankitkumar8396836615@gmail.com>
2022-11-28 23:26:14 +0530

    Time: 115 ms (62.47%), Space: 42.3 MB (88.47%) - LeetHub

---
+++ 0205-isomorphic-strings/0205-isomorphic-strings.js
@@ -0,0 +1,21 @@
+/**
+ * @param {string} s
+ * @param {string} t
+ * @return {boolean}
+ */
+var isIsomorphic = function(s, t) {
+    let sHash = {}
+    let tHash = {}
+
+    if(s.length !== t.length) return false;
+
+    for(let i = 0; i < s.length; i++){
+        let charS = s[i];
+        let charT = t[i];
+        if (!sHash[charS]) { sHash[charS] = charT; }
+        if (!tHash[charT]) { tHash[charT] = charS; }
+        if( sHash[charS] !== charT || tHash[charT] !== charS) return false;
+    }
+
+    return true;
+};
